bd043b2bd30f0546067f33aa75e1fc15e4a21afe
Mayara <mayarapedrazoli@gmail.com>
2024-12-06 18:55:32 -0300

    commit inicial

---
+++ src/scripts/engine.js
@@ -0,0 +1,71 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+        lives: document.querySelector("#lives")
+    },
+    values: {
+        hitPosition:0,
+        result:0,
+        currentTime: 10,
+        currentLives: 3,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 700),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+};
+
+function countDown( ) {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime
+
+    if (state.values.currentTime <= 0){
+        state.values.currentLives--;
+        state.view.lives.textContent = "x" + state.values.currentLives;
+        alert("Game Over! Seu resultado foi " + state.values.result);
+        state.values.currentTime = 10
+    
+    }
+
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square =>{
+        square.classList.remove("enemy");
+
+    } ))
+
+let randomNumber = Math.floor(Math.random() * 9);
+let randomSquare = state.view.squares[randomNumber];
+randomSquare.classList.add("enemy");
+state.values.hitPosition = randomSquare.id
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+       square.addEventListener("mousedown", ()=> {
+        if (square.id === state.values.hitPosition) {
+            state.values.result++
+            state.view.score.textContent = state.values.result
+            state.values.hitPosition=null;
+            playSound("hit");
+        }
+        
+       })
+    })
+}
+
+function init () {
+    addListenerHitBox();
+};
+
+init();
